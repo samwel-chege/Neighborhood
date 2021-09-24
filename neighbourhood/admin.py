a298a76d71381f22f796d3ebde8f13277face63f
samwel-chege <samwelchegeh09@gmail.com>
2021-09-25 00:00:32 +0300

    fix a typo

--- neighbourhood/admin.py
+++ neighbourhood/admin.py
@@ -3,6 +3,6 @@ from .models import Admin,Neighborhood,Residents,Business
 
 # Register your models here.
 admin.site.register(Admin)
-admin.sute.register(Neighborhood)
+admin.site.register(Neighborhood)
 admin.site.register(Residents)
 admin.site.register(Business)
