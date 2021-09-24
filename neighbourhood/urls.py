984cec329e0f2127eb6ec1d920d6b15c4746b58a
samwel-chege <samwelchegeh09@gmail.com>
2021-09-24 15:44:22 +0300

    add a newsletter path

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 from .import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path(r'^ajax/newsletter/$', views.newsletter, name='newsletter'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
