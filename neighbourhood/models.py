2ccdf2c301cc63e2cdb91b491891ab779d63c23d
samwel-chege <samwelchegeh09@gmail.com>
2021-09-24 23:56:13 +0300

    create model classes

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,6 +1,83 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 class NewsLetterRecipients(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=100)
     email = models.EmailField()
+
+class Admin(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    profile_photo = models.ImageField(upload_to='photos')
+
+    def __str__(self):
+        return self.user.username    
+
+class Neighborhood(models.Model):
+    name  = models.CharField(max_length=100) 
+    location = models.CharField(max_length=100)  
+    residents = models.IntegerField(default=1)
+    admin = models.ForeignKey(Admin,on_delete=CASCADE)
+
+    def save_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def search_neighbors(cls,search_term):
+        return cls.objects.filter(name__icontains = search_term)
+
+    @classmethod
+    def update_neighborhood(cls,id,residents):
+        return cls.objects.filter(id=id,residents=residents)    
+
+    def __str__(self):
+        return self.name
+
+class Residents(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    profile_photo = models.ImageField(upload_to='photos')
+    neighborhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE)
+
+    def save_residents(self):
+        self.save()
+
+    def delete_residents(self):
+        self.delete()
+
+    @classmethod
+    def search_resident(cls,search_term):
+        return cls.objects.filter(user__username__icontains =search_term)        
+
+    def __str__(self):
+        return self.user.username
+class Business(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,null=True)
+    neighborhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE)
+    email_address = models.EmailField(max_length=100)
+
+    def save_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    @classmethod
+    def search_business(cls,search_term):
+        return cls.objects.filter(name__icontains = search_term)        
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+
