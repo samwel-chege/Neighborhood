b8e6a1cdba8a2d9cb9e93760cf32fbf8aec84779
samwel-chege <samwelchegeh09@gmail.com>
2021-09-25 00:13:42 +0300

    register the Resident model

--- neighbourhood/admin.py
+++ neighbourhood/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Admin,Neighborhood,Residents,Business
+from .models import Admin,Neighborhood,Resident,Business
 
 # Register your models here.
 admin.site.register(Admin)
 admin.site.register(Neighborhood)
-admin.site.register(Residents)
+admin.site.register(Resident)
 admin.site.register(Business)
