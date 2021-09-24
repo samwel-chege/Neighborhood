152ed36b622abae8d3eb71afad9dcfb6884a7a7a
samwel-chege <samwelchegeh09@gmail.com>
2021-09-24 23:59:17 +0300

    register the models in the admin page

--- neighbourhood/admin.py
+++ neighbourhood/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Admin,Neighborhood,Residents,Business
 
 # Register your models here.
+admin.site.register(Admin)
+admin.sute.register(Neighborhood)
+admin.site.register(Residents)
+admin.site.register(Business)
