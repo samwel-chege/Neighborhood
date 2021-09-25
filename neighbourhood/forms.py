91f66b36edc29f846eceb50c5d37798098fbaba0
samwel-chege <samwelchegeh09@gmail.com>
2021-09-25 23:16:19 +0300

    createa a form to add new residents

--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -3,3 +3,8 @@ from django import forms
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label = 'First_name',max_length=30)
     email = forms.EmailField(label='Email')
+
+class AddResidentForm(forms.Form):
+    name = forms.CharField(label='Resident name',max_length=100)
+    username = forms.CharField(label='username',max_length=100)
+    email = forms.EmailField()    
