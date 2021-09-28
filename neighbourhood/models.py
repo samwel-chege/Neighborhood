55aea372ab99a3099dbc297cf3022b4de82f4145
samwel-chege <samwelchegeh09@gmail.com>
2021-09-28 07:56:26 +0300

    create post model class

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,3 +1,4 @@
+import neighbourhood
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
@@ -19,21 +20,22 @@ class Neighborhood(models.Model):
     name  = models.CharField(max_length=100) 
     location = models.CharField(max_length=100)  
     residents = models.IntegerField(default=1)
-    admin = models.ForeignKey(Admin,on_delete=CASCADE)
+    admin = models.ForeignKey(Admin,on_delete=models.CASCADE)
+    
 
-    def save_neighborhood(self):
-        self.save()
+    # def save_neighborhood(self):
+    #     self.save()
 
-    def delete_neighborhood(self):
-        self.delete()
+    # def delete_neighborhood(self):
+    #     self.delete()
 
-    @classmethod
-    def search_neighbors(cls,search_term):
-        return cls.objects.filter(name__icontains = search_term)
+    # @classmethod
+    # def search_neighbors(cls,search_term):
+    #     return cls.objects.filter(name__icontains = search_term)
 
-    @classmethod
-    def update_neighbors(cls,id,resident):
-        return cls.objects.filter(id=id,residents=resident)    
+    # @classmethod
+    # def update_neighbors(cls,id,resident):
+    #     return cls.objects.filter(id=id,residents=resident)    
 
     def __str__(self):
         return self.name
@@ -42,7 +44,7 @@ class Resident(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     profile_photo = models.ImageField(upload_to='photos')
-    neighborhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE)
 
     def save_residents(self):
         self.save()
@@ -58,6 +60,7 @@ class Resident(models.Model):
         return self.user.username
 class Business(models.Model):
     name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='photos',default='no photo')
     user = models.OneToOneField(User,on_delete=models.CASCADE,null=True)
     neighborhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE)
     email_address = models.EmailField(max_length=100)
@@ -76,7 +79,12 @@ class Business(models.Model):
         return self.name
 
 
-
+class Post(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    post = models.TextField()
+    neighbourhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE)
+    post_date = models.DateTimeField(auto_now_add=True)
 
 
 
