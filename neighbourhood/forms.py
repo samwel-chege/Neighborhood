dfce636c6bf8803827eeb036d1a33484e18af13f
samwel-chege <samwelchegeh09@gmail.com>
2021-09-28 07:55:51 +0300

    create a form for posts

--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -1,10 +1,26 @@
+from neighbourhood.models import Neighborhood,Post
 from django import forms
 
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label = 'First_name',max_length=30)
     email = forms.EmailField(label='Email')
 
+class AdminProfileForm(forms.ModelForm):
+    class Meta:
+        model = Neighborhood
+        exclude = ['admin','residents']    
+
 class AddResidentForm(forms.Form):
     name = forms.CharField(label='Resident name',max_length=100)
     username = forms.CharField(label='username',max_length=100)
     email = forms.EmailField()    
+
+class NeighborhoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighborhood
+        exclude = ['admin','residents']    
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user', 'hood')
