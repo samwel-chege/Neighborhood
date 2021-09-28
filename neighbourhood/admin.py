0ecc4b6cb729f5393e4cb0a7a260f8934f8b6af3
samwel-chege <samwelchegeh09@gmail.com>
2021-09-28 07:55:17 +0300

    register post model class

--- neighbourhood/admin.py
+++ neighbourhood/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Admin,Neighborhood,Resident,Business
+from .models import Admin,Neighborhood,Resident,Business,Post
 
 # Register your models here.
 admin.site.register(Admin)
 admin.site.register(Neighborhood)
 admin.site.register(Resident)
 admin.site.register(Business)
+admin.site.register(Post)
