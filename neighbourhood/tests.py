d52da43fc501868c0aca23abe7cf45df410d5233
samwel-chege <samwelchegeh09@gmail.com>
2021-09-30 01:03:11 +0300

    create test files

--- neighbourhood/tests.py
+++ neighbourhood/tests.py
@@ -1,3 +1,62 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Neighborhood, Profile, Amenity, Post, Neighbourhood, Resident, Business
 
-# Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.sammie = User(username = "sammie", email = "sammie@gmail.com",password = "1234")
+        self.profile = Profile(user= self.sammie)
+        self.sammie.save()
+        self.profile.save()
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.sammie, User))
+        self.assertTrue(isinstance(self.profile, Profile))
+
+class ResidentTestClass(TestCase):
+    def setUp(self):
+        self.sammie = User(username = "sammie", email = "sammie@gmail.com",password = "1234")
+        self.resident = Resident(user= self.sammie, profile_photo='mepng')
+        self.sammie.save()
+        self.resident.save()
+
+    def tearDown(self):
+        Resident.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.sammie, User))
+        self.assertTrue(isinstance(self.resident, Resident))
+
+class NeighbourhoodTestClass(TestCase):
+    def setUp(self):
+        self.location = Neighborhood(location= self.loresho,residents=2)
+        self.westlands.save()
+        self.location.save()
+
+    def tearDown(self):
+        Neighborhood.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.loresho, Neighborhood))
+
+class BusinessTestClass(TestCase):
+    def setUp(self):
+        self.sammie = User(username = "sammie", email = "sammie@gmail.com",password = "1234")
+        self.business = Business(user= self.sammie, photo='mepng')
+        self.sammie.save()
+        self.business.save()
+
+    def tearDown(self):
+        Business.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.sammie, User))
+        self.assertTrue(isinstance(self.business, Business))    
+
+        
