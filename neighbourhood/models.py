91882c704f73d209b00ee69818edd3712a2adcf0
samwel-chege <samwelchegeh09@gmail.com>
2021-09-29 15:52:58 +0300

    update the user column on the admin table

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -10,7 +10,7 @@ class NewsLetterRecipients(models.Model):
 
 class Admin(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,default="Names")
     profile_photo = models.ImageField(upload_to='photos')
 
     def __str__(self):
