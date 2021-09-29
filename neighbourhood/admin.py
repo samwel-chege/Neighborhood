052fcdcf8043d42ab4fbd1ab8a10beb5e143a19a
samwel-chege <samwelchegeh09@gmail.com>
2021-09-29 23:03:16 +0300

    register the Profile model class

--- neighbourhood/admin.py
+++ neighbourhood/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Admin,Neighborhood,Resident,Business,Post
+from .models import Profile,Neighborhood,Resident,Business,Post
 
 # Register your models here.
-admin.site.register(Admin)
+admin.site.register(Profile)
 admin.site.register(Neighborhood)
 admin.site.register(Resident)
 admin.site.register(Business)
